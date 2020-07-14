f35737c0c88e055b676a94996bfe1bc171f29631
Charley Wu <akunzai@gmail.com>
2020-07-14 15:33:58 +0800

    Add InteractiveConsoleAuthorizer implementation

---
+++ src/GSS.Authorization.OAuth/InteractiveConsoleAuthorizer.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Diagnostics;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Options;
+
+namespace GSS.Authorization.OAuth
+{
+    public class InteractiveConsoleAuthorizer : AuthorizerBase
+    {
+        public InteractiveConsoleAuthorizer(IOptions<AuthorizerOptions> options, HttpClient httpClient, IRequestSigner signer) : base(options, httpClient, signer)
+        {
+        }
+
+        public override Task<string> GetVerificationCodeAsync(Uri authorizationUri, CancellationToken cancellationToken = default)
+        {
+            OpenBrowser(authorizationUri);
+            var verificationCode = string.Empty;
+            while (string.IsNullOrWhiteSpace(verificationCode))
+            {
+                Console.Write("Please complete login and authorization in browser and paste the verification code: ");
+                verificationCode = Console.ReadLine();
+            }
+            return Task.FromResult(verificationCode);
+        }
+
+        // https://github.com/dotnet/runtime/issues/17938
+        private void OpenBrowser(Uri uri)
+        {
+            try
+            {
+                Process.Start(new ProcessStartInfo
+                {
+                    FileName = uri.AbsoluteUri,
+                    UseShellExecute = true
+                });
+            }
+            catch
+            {
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux))
+                {
+                    Process.Start("xdg-open", uri.AbsoluteUri);
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+    }
+}
